7a79c80b54177820069692cb023337f0771faa27
Tom X. Tobin <tomxtobin@tomxtobin.com>
2012-09-01 20:09:09 -0400

    Fix parser #setDefaults and add tests
    ActionContainer#setDefaults takes an object as its argument, but was treating
    it as an array.

--- test/sub_commands.js
+++ test/sub_commands.js
@@ -11,6 +11,8 @@ describe('ArgumentParser', function () {
   describe('sub-commands', function () {
     var parser;
     var args;
+    var c1;
+    var c2;
 
     beforeEach(function() {
       parser = new ArgumentParser({debug: true});
@@ -18,10 +20,10 @@ describe('ArgumentParser', function () {
         title:'subcommands',
         dest:"subcommand_name"
       });
-      var c1 = subparsers.addParser('c1', {aliases:['co']});
+      c1 = subparsers.addParser('c1', {aliases:['co']});
       c1.addArgument([ '-f', '--foo' ], {});
       c1.addArgument([ '-b', '--bar' ], {});
-      var c2 = subparsers.addParser('c2', {});
+      c2 = subparsers.addParser('c2', {});
       c2.addArgument([ '--baz' ], {});
     });
 
@@ -62,6 +64,17 @@ describe('ArgumentParser', function () {
         function () {parser.parseArgs([]); },
         /too few arguments/
       );
+    });
+
+    it("should support #setDefaults", function() {
+      c1.setDefaults({spam: 1});
+      c2.setDefaults({eggs: 2});
+      args = parser.parseArgs(['c1']);
+      assert.equal(args.spam, 1);
+      assert.strictEqual(args.eggs, undefined);
+      args = parser.parseArgs(['c2']);
+      assert.equal(args.eggs, 2);
+      assert.strictEqual(args.spam, undefined);
     });
   });
 });
