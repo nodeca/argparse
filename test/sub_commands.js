8589be4a554a3f35244252610fbba0c541d815cf
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-10-14 15:23:03 +0400

    Fixed choices arguments #16

--- test/sub_commands.js
+++ test/sub_commands.js
@@ -60,10 +60,10 @@ describe('ArgumentParser', function () {
       });
     });
 
-    it("should drop down with 'unknown parser' error if parse unrecognized command", function () {
+    it("should drop down with 'Invalid choice' error if parse unrecognized command", function () {
       assert.throws(
         function () {parser.parseArgs('command --baz 1'.split(' ')); },
-        /Unknown parser/
+        /Invalid choice:/
       );
     });
 
