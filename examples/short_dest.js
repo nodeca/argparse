40183eced0ec4495342f9cfea7d81f875dad7242
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-12-14 08:56:48 +0400

    fixes lint & comments for #23

--- examples/short_dest.js
+++ examples/short_dest.js
@@ -7,10 +7,10 @@ var parser = new ArgumentParser({
   addHelp: true,
   description: 'Argparse examples: inferring the option dest'
 });
-parser.addArgument(['-f', '--foo']); // from longoption
-parser.addArgument(['-g']);  // from short option
-parser.addArgument(['-x'],{dest:'xxx'}); // from dest keyword
-parser.addArgument(['--foo-bar']); // '-' to '_'
+parser.addArgument(['-f', '--foo']);        // from long option
+parser.addArgument(['-g']);                 // from short option
+parser.addArgument(['-x'], { dest: 'xxx' });// from dest keyword
+parser.addArgument(['--foo-bar']);          // '-' to '_'
 
 parser.printHelp();
 console.log('-----------');
@@ -18,9 +18,9 @@ console.log('-----------');
 var args;
 args = parser.parseArgs(['-f', '1']);
 console.dir(args);
-args = parser.parseArgs(['-g','2']);
+args = parser.parseArgs(['-g', '2']);
 console.dir(args);
-args = parser.parseArgs(['-f',1,'-g',2,'-x',3,'--foo-bar','FOO BAR']);
+args = parser.parseArgs(['-f', 1, '-g', 2, '-x', 3, '--foo-bar', 'FOO BAR']);
 console.dir(args);
 // expect: { foo: 1, g: 2, xxx: 3, foo_bar: 'FOO BAR' }
 
