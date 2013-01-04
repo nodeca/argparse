2a6ab2ec5257897345350cd9c67878cb17a0b86a
hpaulj <hpj3@myuw.net>
2013-01-03 19:04:50 -0800

    This argparse failed the test_argparse.py TestOptionLike class Args like '-x -2.5 a' are supposed to fail. The problem is that Python match() starts the match at the start of a string. Javascript match can match anywhere in the string. Correction in argument_parser.js, matchArguments() and matchArgumentsPartial() is to add '^' to the pattern.
    TestOptionLike.js tests the change in matchArguments().  I don't know of a test
    for the matchArgumentsPartial() change.

---
+++ test/testoptionlike.js
@@ -0,0 +1,49 @@
+/*global describe, it*/
+
+'use strict';
+
+var assert = require('assert');
+
+var ArgumentParser = require('../lib/argparse').ArgumentParser;
+describe('ArgumentParser', function () {
+  describe('....', function () {
+    var parser;
+    var args;
+
+    it("TestOptionLike", function () {
+      // Tests options that may or may not be arguments
+      parser = new ArgumentParser({debug: true});
+      parser.addArgument(['-x'], {type: 'float'});
+      parser.addArgument(['-3'], {type: 'float', dest: 'y'});
+      parser.addArgument(['z'], {nargs: '*'});
+      
+      args = parser.parseArgs([]);
+      assert.deepEqual(args, {x: null, y: null, z: []});
+      args = parser.parseArgs('-x 2.5 a'.split(' '));
+      assert.deepEqual(args, {x: 2.5, y: null, z: ['a']});
+      args = parser.parseArgs('-3 1 a'.split(' '));
+      assert.deepEqual(args, {x: null, y: 1.0, z: ['a']});
+      
+      assert.throws(function () {
+        parser.parseArgs('-x -2.5'.split(' '));
+      },
+      /expected one argument/i
+      );
+      assert.throws(function () {
+        parser.parseArgs('-x -2.5 a'.split(' '));
+      },
+      /expected one argument/i
+      );
+      // problem with missing '^' in pattern match
+      assert.throws(function () {
+        parser.parseArgs('-3 -1 a'.split(' '));
+      },
+      /expected one argument/i
+      );
+    });
+  });
+});
+
+/*
+
+*/
