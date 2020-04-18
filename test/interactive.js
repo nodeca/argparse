4ed1df679f20fea4a58ed0c57c8297af3d6d8208
Ruby Allison Rose <jtimmerman32@gmail.com>
2020-04-17 23:31:53 -0700

    style(tests): Standardized format via eslint.
    As a side note, for some reason eslint was trying to tell me
    that I had lines longer than 120 characters when my preffered line
    length is set to 80 in my editor and I usually never exceede that.
    
    Apparently after some fiddling around with it, it seems the issue
    was with my indentation format: I use hard tabs by default.
    I'll probably file a bug report with ESLint to verify / fix, but
    just in case I forget I'll leave this in the commit history.
    
    Beware the crusty `--fix`, it'll break yer code m8ies.

--- test/interactive.js
+++ test/interactive.js
@@ -1,3 +1,4 @@
+/*global describe, it*/
 /***
  * Tests cases where argument parser instance was generated within an
  * interactive NodeJS instance rather than from a script NodeJS is executing.
@@ -6,11 +7,7 @@
 'use strict';
 
 var subprocess = require('child_process');
-var assert = require('assert');
 var path = require('path');
-var fs = require('fs');
-
-var ArgumentParser = require('../lib/argparse').ArgumentParser;
 
 /**
  * Does it's best to imitate interactively playing with node.
@@ -18,35 +15,35 @@ var ArgumentParser = require('../lib/argparse').ArgumentParser;
  * REPL model.
  *
  * TODO:
- *	* Add time dependant input to include capture potential for special,
- *	  conditions like racing. Had to make things sync to accomodate for
- *    old NodeJS version support.
+ * Add time dependant input to include capture potential for special,
+ * conditions like racing. Had to make things sync to accomodate for
+ * old NodeJS version support.
  */
-function interactive(script){
-	// Wraps the script in a try loop that catches thrown javascript errors
+function interactive(script) {
+  // Wraps the script in a try loop that catches throws javascript errors
 	// so they aren't ignored unless the script application processes them.
-	script = "try{ "+ script +" } catch (e){ console.error(e); process.exit(1) }";
-	script = script + "\n.exit;";  // don't forget to exit when done.
+  script = 'try{ '+ script +' } catch (e){ console.error(e); process.exit(1) }';
+  script += '\n.exit;';  // don't forget to exit when done.
 
-	var node = subprocess.spawnSync("node", ["-i"], {
-		// Puts all of our interactive node sessions into the root directory of the
-		// repository to make sure we can find all our node modules automatically.
-		pwd: path.resolve(__dirname, "../"),
+  var node = subprocess.spawnSync('node', [ '-i' ], {
+    // Puts all of our interactive node sessions into the root directory of the
+    // repository to make sure we can find all our node modules automatically.
+    pwd: path.resolve(__dirname, '../'),
 
-		// Just shove script directly into the standard input for the program.
-		input: script
-	});
+    // Just shove script directly into the standard input for the program.
+    input: script
+  });
 
-	if (node.status !== 0 && !node.signal) {
-		throw Error(node.stderr);
-	}
+  if (node.status !== 0 && !node.signal) {
+    throw Error(node.stderr);
+  }
 }
 
-describe('Interactive NodeJS', () => {
-	it('Can instantiate empty ArgumentParser interactively', function (){
-		return interactive(
-			"var ArgumentParser = require('./lib/argparse').ArgumentParser;" +
-			"var parser = new ArgumentParser();"
-		);
-	});
+describe('Interactive NodeJS', function () {
+  it('Can instantiate empty ArgumentParser interactively', function () {
+    return interactive(
+      'var ArgumentParser = require("./lib/argparse").ArgumentParser;' +
+      'var parser = new ArgumentParser();'
+    );
+  });
 });
