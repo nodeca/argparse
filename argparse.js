e6e7a82f8f40819bca3cd1feadf8ea361d94b18a
Akshar Patel <akshar.patel.47@gmail.com>
2020-12-04 19:19:11 -0700

    throw error if exit_on_error is false

--- argparse.js
+++ argparse.js
@@ -3653,9 +3653,12 @@ const ArgumentParser = _camelcase_alias(_callable(class ArgumentParser extends _
         if (this.debug === true) throw new Error(message)
         // end
         this.print_usage(process.stderr)
+        let args = {prog: this.prog, message: message}
+        const errorMessage = sub('%(prog)s: error: %(message)s\n', args);
         if (this.exit_on_error) {
-            let args = {prog: this.prog, message: message}
-            this.exit(2, sub('%(prog)s: error: %(message)s\n', args))
+            this.exit(2, errorMessage)
+        } else {
+            throw new Error(errorMessage);
         }
     }
 }))
