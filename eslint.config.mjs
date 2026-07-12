5b0f9cdbe1d33c9983fdc69c872ba09ddf5254c0
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-07-12 09:06:16 +0300

    lint => neostandard

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import neostandard from 'neostandard'
+
+export default [
+    ...neostandard(),
+    {
+        rules: {
+            '@stylistic/array-bracket-spacing': 'off',
+            '@stylistic/indent': 'off',
+            '@stylistic/lines-between-class-members': 'off',
+            '@stylistic/new-parens': 'off',
+            '@stylistic/no-mixed-operators': 'off',
+            '@stylistic/no-multi-spaces': 'off',
+            '@stylistic/no-multiple-empty-lines': 'off',
+            '@stylistic/object-curly-newline': 'off',
+            '@stylistic/object-curly-spacing': 'off',
+            '@stylistic/object-property-newline': 'off',
+            '@stylistic/operator-linebreak': 'off',
+            '@stylistic/padded-blocks': 'off',
+            '@stylistic/quotes': 'off',
+            '@stylistic/space-before-function-paren': 'off',
+            '@stylistic/space-infix-ops': 'off',
+            '@stylistic/spaced-comment': 'off',
+            '@stylistic/yield-star-spacing': 'off',
+            camelcase: 'off',
+            'no-control-regex': 'off',
+            'no-regex-spaces': 'off',
+            'no-undef-init': 'off',
+            'no-unmodified-loop-condition': 'off',
+            'no-useless-constructor': 'off',
+            'no-useless-escape': 'off',
+            'one-var': 'off',
+            'prefer-const': 'off'
+        }
+    }
+]
